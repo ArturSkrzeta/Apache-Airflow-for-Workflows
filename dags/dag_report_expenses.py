978ed96a1c4d6a6d03f95cd724dbe79656f28b42
ArturSkrzeta <65757653+ArturSkrzeta@users.noreply.github.com>
2021-10-31 22:22:15 +0100

    Update dag_report_expenses.py

--- dags/dag_report_expenses.py
+++ dags/dag_report_expenses.py
@@ -1,6 +1,5 @@
 # Importing Airflow's features
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 
